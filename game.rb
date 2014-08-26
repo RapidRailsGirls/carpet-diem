ba60d087a8c2572201ef8c3b013ad6361ad6ee44
Magdalena <madziaf86@tlen.pl>
2014-08-26 15:15:02 +0200

    showed carpet on the screen

--- game.rb
+++ game.rb
@@ -3,17 +3,34 @@ require 'gosu'
 class Window < Gosu::Window
 
   def initialize
-    super(800, 600, false)
+    super(1024, 768, false)
+    @carpet = Carpet.new(self)
   end
 
   def draw
-
+    @carpet.draw
   end
 
   def update
 
   end
 
+  
+end
+
+class Carpet
+
+  attr_accessor :x
+
+  def initialize(window)
+    @image = Gosu::Image.new(window, "media/carpet.png")
+    @x = 1024/2 - 150/2
+    @y = 768/2 - 192/2
+  end
+  
+  def draw
+    @image.draw(@x, @y, 0)
+  end
 end
 
 window = Window.new
