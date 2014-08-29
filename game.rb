33ce052b004f2224453eec38a713e520c126228e
BriocheBerlin <bri.ma@gmx.de>
2014-08-29 11:54:49 +0200

    Background scrolls

--- game.rb
+++ game.rb
@@ -3,16 +3,33 @@ WINDOW_HEIGHT = 768
 WINDOW_WIDTH  = 1024
 
 class Window < Gosu::Window
+  NUM_TILES = 16
+  TILE_COLS = 4
+  INITIAL_Y = -300
+  VELOCITY = 3
+
+  module YAccessible
+    attr_writer :y
+    def y
+      @y.to_i
+    end
+  end
 
   def initialize
     super(WINDOW_WIDTH, WINDOW_HEIGHT, false)
     @carpet = Carpet.new(self)
-    @backgrounds = 25.times.collect { Gosu::Image.new(self, "media/background.jpeg", true) }
+    @backgrounds = NUM_TILES.times.collect do
+      Gosu::Image.new(self, "media/background.jpeg", true).extend(YAccessible)
+    end
+    @yplus = INITIAL_Y
   end
 
   def draw
     @carpet.draw
-    @backgrounds.each_with_index { | bg, index | bg.draw((index % 5) * bg.width, (index / 5) * bg.height, 0) }
+    @backgrounds.each_with_index do | bg, index |
+      bg.y = (index / TILE_COLS) * bg.height + @yplus
+      bg.draw((index % TILE_COLS) * bg.width, bg.y, 0)
+    end
   end
 
   def update
@@ -24,8 +41,17 @@ class Window < Gosu::Window
       @carpet.x = [@carpet.x + 3, 930].min
       @carpet.flip_right
     end
+    scroll_background
   end
 
+  def scroll_background
+    bg = @backgrounds.last
+    if bg.y >= ((NUM_TILES / TILE_COLS) - 1) * bg.height - VELOCITY
+      @yplus = INITIAL_Y
+    else
+      @yplus += VELOCITY
+    end
+  end
 end
 
 class Carpet
@@ -36,7 +62,7 @@ class Carpet
     @image = @image_right = Gosu::Image.new(window, "media/carpet.png")
     @image_left = Gosu::Image.new(window, "media/carpet_flipped.png")
     @x = WINDOW_WIDTH/2 - @image.width/2
-    @y = WINDOW_HEIGHT/2 - @image.height/2
+    @y = WINDOW_HEIGHT/1.4 - @image.height/2
   end
   
   def draw
