b9c96c5713d9347609ef1c8479975baa31bab084
Kathi Zwick <ka.zwick@gmail.com>
2014-08-29 19:39:16 +0200

    Refactored genie apearing hack

--- game.rb
+++ game.rb
@@ -110,15 +110,45 @@ end
 
 class LampWithGenie
   attr_accessor :y, :x, :rubbed
+  alias :rubbed? :rubbed
+  undef :rubbed
+
+  def good?
+    if defined? @good
+      return @good
+    else
+      @good = rand(2) == 0
+    end
+  end
+
+  def evil?
+    !good?
+  end
+
+  def flipped?
+    if defined? @flipped
+      return @flipped
+    else
+      @flipped = rand(2) == 0
+    end
+  end
 
   def initialize(window)
-    @random_lamp = rand(2)
-    if @random_lamp.odd?
-      @lamp = Gosu::Image.new(window, "media/lamp.png")
-      @genie = Gosu::Image.new(window, "media/good_genie.png")
-    else
+    @scale = 0.0
+    if flipped?
       @lamp = Gosu::Image.new(window, "media/lamp_flipped.png")
-      @genie = Gosu::Image.new(window, "media/good_genie_flipped.png")
+      if good?
+        @genie = Gosu::Image.new(window, "media/good_genie_flipped.png")
+      else
+        @genie = Gosu::Image.new(window, "media/evil_genie_flipped.png")
+      end
+    else
+      @lamp = Gosu::Image.new(window, "media/lamp.png")
+      if good?
+        @genie = Gosu::Image.new(window, "media/good_genie.png")
+      else
+        @genie = Gosu::Image.new(window, "media/evil_genie.png")
+      end
     end
     @rubbed = false
     @x = rand(@genie.width/2..(WINDOW_WIDTH - @genie.width))
@@ -127,11 +157,16 @@ class LampWithGenie
 
   def draw
     @lamp.draw(@x, @y, 2)
-    if rubbed
-      if @random_lamp.odd?
-        @genie.draw(@x - 150, @y - 200, 3)
-      else
-        @genie.draw(@x + 140, @y - 200, 3)
+    if rubbed?
+      @scale += 0.03 if @scale <= 1
+      if !flipped? && good?
+        @genie.draw(@x - 150 * @scale, @y - 200 * @scale, 3, @scale, @scale)
+      elsif !flipped? && evil?
+        @genie.draw(@x - 150 * @scale, @y - 200 * @scale, 3, @scale, @scale)
+      elsif flipped? && good?
+        @genie.draw(@x + 150, @y - 200 * @scale, 3, @scale, @scale)
+      elsif flipped? && evil?
+        @genie.draw(@x + 120, @y - 200 * @scale, 3, @scale, @scale)
       end
     end
   end
