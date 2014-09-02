1ff44da54830a1dc52a051b184a0235c5354ae50
Erik Michaels-Ober <sferik@gmail.com>
2014-09-02 16:07:51 +0200

    Remove subject because it's confusing

--- spec/carpet_spec.rb
+++ spec/carpet_spec.rb
@@ -4,20 +4,21 @@ require_relative '../carpet'
 
 describe Carpet do
 
-  subject do
-    window = Gosu::Window.new(9, 9, false)
-    Carpet.new(window, 'spec/fixtures/capital_i.png', 'spec/fixtures/capital_i.png')
+  let(:window) do
+    Gosu::Window.new(9, 9, false)
   end
 
   describe '#x' do
     it 'should start horizontally centered' do
-      expect(subject.x).to eq(4)
+      carpet = Carpet.new(window, 'spec/fixtures/capital_i.png', 'spec/fixtures/capital_i.png')
+      expect(carpet.x).to eq(4)
     end
   end
 
   describe '#y' do
     it 'should start in the lower three-fifths of the window' do
-      expect(subject.y).to eq(5)
+      carpet = Carpet.new(window, 'spec/fixtures/capital_i.png', 'spec/fixtures/capital_i.png')
+      expect(carpet.y).to eq(5)
     end
   end
 
