9d2cb8c87ba6e78824d0e33b1e0beac3d6485ebe
Kathi Zwick <ka.zwick@gmail.com>
2014-09-22 13:43:28 +0200

    Use new endboss image
    closes #13

--- endboss.rb
+++ endboss.rb
@@ -1,15 +1,15 @@
 class Endboss
 
   def initialize(window)
-    @image = Gosu::Image.new(window, 'media/evil_genie_flipped.png')
-    @x = 200
-    @y = 700
+    @image = Gosu::Image.new(window, 'media/endboss.png')
+    @x = window.width/2 - @image.width/2
+    @y = window.height
     @scale = 0.0
   end
 
   def draw
     @scale += 0.03 if @scale <= 1
-    @image.draw(@x, @y - 600 * @scale, 3, 3.5 * @scale, 3.5 * @scale)
+    @image.draw(@x, @y - 750 * @scale, 3, @scale, @scale)
   end
 
 end
